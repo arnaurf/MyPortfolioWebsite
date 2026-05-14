cf7fe0819961e7c5533da6d73bed56ed1e8e29e3
Arnau Ruiz <arnauruiz1998@gmail.com>
2026-05-14 20:46:34 +0200

    Add favicons

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, Google
 urlpatterns = [
     path('', HomePage),
-
+    path('google15457510d2cba688.html', Google),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def HomePage(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+def Google(request, *args, **kwargs):
+    return render(request, 'frontend/google15457510d2cba688.html')
