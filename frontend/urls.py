efdc120f3c1335262cc9efb72bf7c28e707ee36a
Arnau Ruiz <arnauruiz1998@gmail.com>
2026-05-14 21:55:21 +0200

    Add better sections behaviour keeping SEO

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from .views import HomePage, Google, Biography, Skills, Experience, Projects, Contact
+from .views import HomePage, Google
 urlpatterns = [
     path('', HomePage),
     path('google15457510d2cba688.html', Google),
