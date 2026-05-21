2df899324d2524044fc7b8282951fdd8f80db737
Arnau Ruiz <arnauruiz1998@gmail.com>
2026-05-21 21:04:49 +0200

    Create robots.txt

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, re_path
 from django.views.generic.base import TemplateView
 from .views import HomePage, Google
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Allow: /",
+        "",
+        "User-agent: GPTBot",
+        "Disallow: /",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+
 urlpatterns = [
     path('', HomePage),
     path('google15457510d2cba688.html', Google),
